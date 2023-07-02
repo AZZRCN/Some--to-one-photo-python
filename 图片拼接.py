447c4983c663dbc6762ab4e7ae10cc68036e9904
AZZRCN <qazwsx233343@163.com>
2023-07-02 11:20:36 +0800

    tRUEC
    支持横竖拼接,关于图像分辨率请参考最小公共倍数.py的内容

--- 图片拼接.py
+++ 图片拼接.py
@@ -1,9 +1,6 @@
 #from io import BytesIO
 import tkinter as tk
-from tkinter import Button, Entry
 from tkinter import filedialog
-import os
-from typing import List
 from PIL import Image
 from tkinter.messagebox import askyesnocancel
 from 最小公倍数 import lcm
@@ -11,71 +8,32 @@ root = tk.Tk()
 imagelist:list[str] = []
 successlater=["jpeg","png","jpg","ico","bmp","tiff"]
 label = tk.Label(root)
-label.grid(column=1, row=1, columnspan=3)
+label.grid(column=1, row=1, columnspan=5)
 
 
 def update():
     temp = ""
     for i in imagelist:
-        temp += (str(i)+"\n")
+        temp += (i + "\n")
     if (temp == ""):
-        temp = "这里还没有东西哦"
-    else:
-        temp = temp[:-1]
-    label.configure(text=temp)
+        temp = "这里还没有东西哦\n"
+    label.configure(text=temp[:-1])
     del temp
-    if (len(imagelist) == 0):
-        test3["state"] = "disabled"
-    else:
-        test3["state"] = "normal"
 
 
-def instr():
-    global test, test2, test3
-    test["state"] = "disabled"
-    test2["state"] = "disabled"
-    test3["state"] = "disabled"
-    enter = Entry(root)
-    enter.grid(column=1, row=3, columnspan=3)
-    def incomplate():
-        nonlocal acceptButton, helpfindButton, enter
-        global test, test2, test3
-        t = enter.get()
-        if (os.access(t, os.F_OK) and t.split(".")[-1] in successlater):
-            test["state"] = "normal"
-            test2["state"] = "normal"
-            test3["state"] = "normal"
-            acceptButton.destroy()
-            del acceptButton
-            helpfindButton.destroy()
-            del helpfindButton
-            enter.destroy()
-            del enter
-            imagelist.append(t)
-            update()
-        else:
-            enter.delete('0', 'end')
-            enter.insert("0", "错误的路径")
-    acceptButton = Button(root, text="提交", command=incomplate)
-    acceptButton.grid(column=1, row=4)
-    def helpfind():
-        t = filedialog.askopenfilenames()  # str
-        if (not all(t)):
-            return
-        if(type(t) == str):
-            enter.delete('0', 'end')
-            enter.insert('0', t[2:-3])
-        else:
-            for i in t:
-                imagelist.append(i)
-            update()
-    helpfindButton = Button(root, text="帮助寻找", command=helpfind)
-    helpfindButton.grid(column=2, row=4)
-
-
-def delobj():
-    del imagelist[len(imagelist)-1]
+def helpfind():
+    t = filedialog.askopenfilenames()  # str
+    for i in t:
+        imagelist.append(i)
     update()
+
+
+def delobj(delall:bool = False):
+    global imagelist
+    if(len(imagelist) != 0):
+        if(not delall):del imagelist[len(imagelist)-1]
+        elif(delall == True):imagelist = []
+        update()
 def makeobj():
     #ansimage = Image.new('RGB',(20,20))
     idea = askyesnocancel("设置模式","是->宽\n否->高")
@@ -104,22 +62,34 @@ def makeobj():
     else:
         temph = []
         for i in imagelist:
-            temph.append(Image.open(i).size[1])
+            t = Image.open(i, "r").convert('RGB')  # 读取并指定模式为RGB
+            temph.append(t.size[1])
         h = lcm(temph)
+        cnt = 0
+        tw = []
+        tl = []
         for i in imagelist:
-            t = Image.open(i,"r")
-            tl = t.size
-            t.resize((h,int(h/tl[1]*tl[0]))).show()
+            t = Image.open(i, "r").convert('RGB')  # 读取并指定模式为RGB
+            tn = t.size
+            tl.append(t.resize((round(h / tn[1] * tn[0]), h)))  # 转换为整数，四舍五入
+            tw.append(round(h / tn[1] * tn[0]))
+        ansimage = Image.new('RGB', (sum(tw), h))
+        for i in tl:
+            ansimage.paste(i, (sum(tw[:cnt]), 0))  # 粘贴位置更改为 (sum(tw￼), 0)
+            cnt += 1
+        ansimage.show()
 
 test = tk.Button(root, text="update", command=update)
 test.grid(column=2, row=2)
-test2 = tk.Button(root, text="write", command=instr)
+test2 = tk.Button(root, text="write", command=helpfind)
 test2.grid(column=1, row=2)
 test3 = tk.Button(root, text="delete", command=delobj)
 test3.grid(column=3, row=2)
-update()
 test4=tk.Button(root, text="make", command=makeobj)
 test4.grid(column=4, row=2)
+test5 = tk.Button(root, text="delall", command=lambda:delobj(True))
+test5.grid(column=5, row=2)
+update()
 root.mainloop()
 
 """import numpy as np
